765e800ae1121247f4ca1df66b41734d541f243b
putin27 <putin27@livemail.tw>
2016-04-28 23:16:55 +0800

    DBHelper修正： 1.更改table的名子 2.新增tagTable創建指令

--- app/src/main/java/com/example/amber/dinnereatwhat/DBHelper.java
+++ app/src/main/java/com/example/amber/dinnereatwhat/DBHelper.java
@@ -12,14 +12,29 @@ import java.util.ArrayList;
 //table的第一筆資料 cursor的position從0開始 裡面的_id是第0個欄位 所以資料從1開始取
 
 public class DBHelper extends SQLiteOpenHelper {
-
+    //資料庫名稱
     private final static String DATABASE_NAME = "dinnerDatabase";
+    //資料庫版本
     private final static int DATABASE_VERSION = 1;
-    private String tableName = "dinner";
-    private String createDinnerTable = "CREATE TABLE IF NOT EXISTS " + tableName
+    //dinner table的名稱
+    private String dinnerTable = "dinner";
+    //tag table的名稱
+    private String tagTable = "tag";
+    //創建dinner table的指令
+    //_id int,shop char,meal char,price int
+    private String createDinnerTable = "CREATE TABLE IF NOT EXISTS " + dinnerTable
             + " ("
             + "_id INTEGER PRIMARY KEY AUTOINCREMENT,"
             + "shop CHAR, meal CHAR, price INT,tag CHAR"
+            + ")";
+    //創建tag table的指令
+    //t_id int foreign key,tag
+    //foreign key 要放在最後面宣告
+    private String createTagTable = "CREATE TABLE IF NOT EXISTS " + tagTable
+            + " ("
+            + "t_id INTEGER ,"
+            + "tag CHAR,"
+            + "FOREIGN KEY(t_id) REFERENCES " + dinnerTable + "(_id)"
             + ")";
 
     private SQLiteDatabase db;
@@ -42,7 +57,7 @@ public class DBHelper extends SQLiteOpenHelper {
 
     //判斷是否資料庫為空
     public boolean isEmpty() {
-        cursor = db.query(tableName, null, null, null, null, null, null);
+        cursor = db.query(dinnerTable, null, null, null, null, null, null);
         // 如果沒有第一筆資料 回傳false
         return !cursor.moveToFirst();
 
@@ -54,12 +69,12 @@ public class DBHelper extends SQLiteOpenHelper {
         contentValues.put("meal", dinnerData.meal);
         contentValues.put("price", dinnerData.price);
         contentValues.put("tag", dinnerData.tag);
-        db.insert(tableName, null, contentValues);
+        db.insert(dinnerTable, null, contentValues);
     }
 
     public ArrayList<DinnerData> getAllDinnerData() {
         ArrayList<DinnerData> dinnerDatas = new ArrayList<>();
-        cursor = db.query(tableName, null, null, null, null, null, null);
+        cursor = db.query(dinnerTable, null, null, null, null, null, null);
         cursor.moveToFirst();
         for (int i = 0; i < cursor.getCount(); i++) {
             dinnerDatas.add(new DinnerData(cursor.getString(1), cursor.getString(2), cursor.getInt(3), cursor.getString(4)));
@@ -69,13 +84,13 @@ public class DBHelper extends SQLiteOpenHelper {
     }
 
     public DinnerData getDinnerData(int position) {
-        cursor = db.query(tableName, null, null, null, null, null, null);
+        cursor = db.query(dinnerTable, null, null, null, null, null, null);
         cursor.moveToPosition(position);
         return new DinnerData(cursor.getString(1), cursor.getString(2), cursor.getInt(3), cursor.getString(4));
     }
 
     public int getSize() {
-        cursor = db.query(tableName, null, null, null, null, null, null);
+        cursor = db.query(dinnerTable, null, null, null, null, null, null);
 
         return cursor.getCount();
     }
